8500a08744a1894ca6943cc90dfc2c25eb4bb309
hornliquant <hornliquant@gmail.com>
2026-03-03 18:37:53 -0800

    add ios swiftUI app with name `AppleHealthBridge`

---
+++ AppleHealthBridge/AppleHealthBridge/AppleHealthBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleHealthBridgeApp.swift
+//  AppleHealthBridge
+//
+//  Created by Janice Yan on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleHealthBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleHealthBridge/AppleHealthBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleHealthBridge
+//
+//  Created by Janice Yan on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
